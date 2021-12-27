9178afd284d9ecbf57c387013ab1b78ead962572
EdvardUL <79190923+EdvardUL@users.noreply.github.com>
2021-12-27 21:54:28 +0300

    Add files via upload

---
+++ Laba19.2 KPIAP/Laba19.2 KPIAP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Laba19._2_KPIAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar[] arr = new Tovar[4];
+            arr[0] = new Tovar(12, 11, "a", "abc");
+            arr[1] = new Tovar(12, 11, "a", "abc");
+            arr[2] = new Tovar(13, 10, "b", "qwe");
+            arr[3] = new Tovar(14, 19, "c", "zxc");
+            var zxc = arr.DistinctBy(x => x.Name).ToList();
+            foreach (var item in zxc)
+
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ l19.1 ch/l19.1 ch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l19._1_ch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] mas = new double[n];
+            fillArray(mas);
+            Sort(mas);
+            Console.ReadKey();
+        }
+        static void Sort(double[] arr)
+        {
+            var zxc = arr.Where(X => X < 0).Count();
+            var abc = arr.OrderBy(s => s);
+            foreach (var item in abc)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(zxc);
+        }
+        static void fillArray(double[] arr)
+        {
+            Random rand = new Random();
+            for (int x = 0; x < arr.Length; x++)
+            {
+                arr[x] = rand.Next(-20,20) ;
+                Console.WriteLine(arr[x]);
+            }
+        }
+    }
+}
