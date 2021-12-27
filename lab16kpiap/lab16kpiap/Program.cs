148bb93ef3b5813ba951118bb1d252c1c00562b9
EdvardUL <79190923+EdvardUL@users.noreply.github.com>
2021-12-27 21:52:57 +0300

    Add files via upload

---
+++ lab16kpiap/lab16kpiap/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace lab16kpiap
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamReader f = new StreamReader("test.txt");
+            string s = f.ReadToEnd(), temp;
+            int i = 3, count = 0;
+            while (i < s.Length)
+            {
+                temp = s.Substring(i - 3, 3);
+                Regex regex = new Regex(@temp);
+                MatchCollection matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    if (matches.Count > count)
+                    { count = matches.Count; }
+                }
+                i++;
+            }
+            Console.WriteLine(count);
+            f.Close();
+        }
+    }
+}
