b32d6fdecc2a853a8cda9b335457f77e228f4113
EdvardUL <79190923+EdvardUL@users.noreply.github.com>
2021-12-27 21:50:31 +0300

    Add files via upload

---
+++ lab13kpiap/lab13kpiap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace lab13kpiap
+{
+    class Program
+    {
+        public delegate double Function(double a);
+        static void Main(string[] args)
+        {
+            CountFunc(-1, 1, 0.04, Math.Tan);
+            Console.WriteLine("~~~~~~~~~~~~~~~~");
+            CountCotFunc(-1, 1, 0.04, Math.Tan);
+        }
+        public static void CountFunc(double a, double b,double c, Function F)
+        {
+            for (double i = a; i <= b; i += c)
+            {
+                Console.WriteLine(F(i));
+            }
+        }
+        public static void CountCotFunc(double a, double b, double c, Function F)
+        {
+            for (double i = a; i <= b; i += c)
+            {
+                Console.WriteLine(1/F(i));
+            }
+        }
+    }
+}
---
+++ lab13kpiap/lab14kpiap/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab14kpiap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab14kpiap")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0368ed5f-c329-4ecb-8579-7859f2cb1caa")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
