f5ff7e542f3ad1e29ed4b1b86185785f1206262c
EdvardUL <79190923+EdvardUL@users.noreply.github.com>
2022-01-05 17:37:58 +0300

    Add files via upload

---
+++ l12.2/l11.2/Creature.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace l11._2
+{
+    abstract class Creature
+    {
+        public int affinity = 50;
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        int weight;
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Введите корректное значение");
+                }
+                else
+                {
+                    weight = value;
+                }
+            }
+        }
+        int lvl;
+        public int Lvl
+        {
+            get
+            {
+                return lvl;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Введите корректное значение");
+                }
+                else
+                {
+                    lvl = value;
+                }
+            }
+        }
+        string gender;
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+        }
+        string skillName;
+        public string SkillName
+        {
+            set
+            {
+                skillName = value;
+            }
+            get
+            {
+                return skillName;
+            }
+        }
+        public Creature( string newName, int newWeight, string newGender,string newSkill ,int newLvl)
+        {
+            name = newName;
+            weight = newWeight;
+            gender = newGender;
+            SkillName = newSkill;
+            Lvl = newLvl;
+        }
+        public Creature()
+        {
+            name = "Skk";
+            Weight = 85;
+            gender = "Female";
+            SkillName = "None";
+        }
+        public abstract void Show();
+        public abstract void Enhance();
+        public abstract void LimitBreak();
+    }
+}
---
+++ l12.2/l11.2/CreaturesArray.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace l11._2
+{
+    class CreaturesArray : IList,ICloneable,IComparable
+    {
+        private object[] _contents = new object[7];
+        private int _count;
+        public CreaturesArray()
+        {
+            _count = 0;
+        }
+        public int Add(object value)
+        {
+            if (_count < _contents.Length)
+            {
+                _contents[_count] = value;
+                _count++;
+
+                return (_count - 1);
+            }
+
+            return -1;
+        }
+        public void Clear()
+        {
+            _count = 0;
+        }
+        public bool Contains(object value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_contents[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int IndexOf(object value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_contents[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void Insert(int index, object value)
+        {
+            if ((_count + 1 <= _contents.Length) && (index < Count) && (index >= 0))
+            {
+                _count++;
+
+                for (int i = Count - 1; i > index; i--)
+                {
+                    _contents[i] = _contents[i - 1];
+                }
+                _contents[index] = value;
+            }
+        }
+        public bool IsFixedSize
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public void Remove(object value)
+        {
+            RemoveAt(IndexOf(value));
+        }
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    _contents[i] = _contents[i + 1];
+                }
+                _count--;
+            }
+        }
+        public object this[int index]
+        {
+            get
+            {
+                return _contents[index];
+            }
+            set
+            {
+                _contents[index] = value;
+            }
+        }
+        public void CopyTo(Array array, int index)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                array.SetValue(_contents[i], index++);
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+        public bool IsSynchronized
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public object SyncRoot
+        {
+            get
+            {
+                return this;
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+        public void PrintContents()
+        {
+            Console.WriteLine($"List has a capacity of {_contents.Length} and currently has {_count} elements.");
+            Console.Write("List contents:\n");
+            for (int i = 0; i < Count; i++)
+            {
+                Console.Write($" {_contents[i]}");
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
+        }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public int CompareTo(object obj)
+        {
+            return _count.CompareTo(obj);
+        }
+    }
+}
